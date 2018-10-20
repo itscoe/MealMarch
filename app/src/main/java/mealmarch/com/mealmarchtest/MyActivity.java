4017ded077a181f6a55c48a0498adc73e469b748
TheFinalTzar <44308833+TheFinalTzar@users.noreply.github.com>
2018-10-20 00:49:05 -0500

    Added API key

--- app/src/main/java/mealmarch/com/mealmarchtest/MyActivity.java
+++ app/src/main/java/mealmarch/com/mealmarchtest/MyActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MyActivity extends AppCompatActivity {
 
+    private static String APIKey = "AIzaSyDsc00rP6Gf0HLpoVmz_onQMynA_Z5ErmA";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
