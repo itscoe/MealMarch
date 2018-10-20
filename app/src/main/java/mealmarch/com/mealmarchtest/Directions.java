28500ab082801879216483b1dc540a2f3d67dcb5
itscoe <itslagle@coe.edu>
2018-10-20 09:14:46 -0500

    Implemented Here SDK

--- app/src/main/java/mealmarch/com/mealmarchtest/Directions.java
+++ app/src/main/java/mealmarch/com/mealmarchtest/Directions.java
@@ -3,11 +3,42 @@ package mealmarch.com.mealmarchtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapFragment;
+
 public class Directions extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_directions);
+        // Search for the Map Fragment
+        final MapFragment mapFragment = (MapFragment)
+                getFragmentManager().findFragmentById(R.id.mapfragment);
+        // initialize the Map Fragment and
+        // retrieve the map that is associated to the fragment
+        mapFragment.init(new OnEngineInitListener() {
+            @Override
+            public void onEngineInitializationCompleted(
+                    OnEngineInitListener.Error error) {
+                if (error == OnEngineInitListener.Error.NONE) {
+                    // now the map is ready to be used
+                    Map map = mapFragment.getMap();
+                    // ...
+                } else {
+                    System.out.println("ERROR: Cannot initialize MapFragment");
+                }
+            }
+        });
+        // now the map is ready to be used
+        Map map = mapFragment.getMap();
+
+        // Set the map center to Vancouver, Canada.
+        map.setCenter(new GeoCoordinate(49.196261,
+                -123.004773), Map.Animation.NONE);
     }
 }
