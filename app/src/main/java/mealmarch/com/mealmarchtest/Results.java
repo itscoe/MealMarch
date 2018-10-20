b5d37980852245c9919e968537879205d93c64fe
ebford <38540745+ebford@users.noreply.github.com>
2018-10-20 15:10:37 -0500

    share button and results layout, some functionality might be missing as we need to know what mode it is, restaurant name, and maybe more on the sharing.

--- app/src/main/java/mealmarch/com/mealmarchtest/Results.java
+++ app/src/main/java/mealmarch/com/mealmarchtest/Results.java
@@ -1,7 +1,13 @@
 package mealmarch.com.mealmarchtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Results extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class Results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        final TextView mDistance = findViewById(R.id.Distance_Info);
+        final TextView mTime = findViewById(R.id.Time_Info);
+        TextView mCalorie = findViewById(R.id.Calorie_Info);
+        TextView mThreshold = findViewById(R.id.Threshold_Info);
+        Button mShareButton = findViewById(R.id.ShareButton);
+        Button mShareHCButton = findViewById(R.id.shareHCButton);
+
+        mShareButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                String shareString = ("I covered " + mDistance.getText().toString() + " in " + mTime.getText().toString() + " minutes on my way to eat at ");
+                Intent mSharingIntent = new Intent(Intent.ACTION_SEND);
+                mSharingIntent.setType("text/plain");
+                mSharingIntent.putExtra(Intent.EXTRA_SUBJECT,  "Write your subject here");
+                mSharingIntent.putExtra(Intent.EXTRA_TEXT, shareString);
+                startActivity(Intent.createChooser(mSharingIntent, "Share text via"));
+            }
+        });
+
+        mShareHCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String shareString = ("I challenged the hot and cold mode and covered " + mDistance.getText().toString() + " in " + mTime.getText().toString() + " minutes and got a coupon to eat at ");
+                Intent mSharingIntent = new Intent(Intent.ACTION_SEND);
+                mSharingIntent.setType("text/plain");
+                mSharingIntent.putExtra(Intent.EXTRA_SUBJECT,  "Write your subject here");
+                mSharingIntent.putExtra(Intent.EXTRA_TEXT, shareString);
+                startActivity(Intent.createChooser(mSharingIntent, "Share text via"));
+            }
+        });
     }
 }
