9791de16795ec17b70ffe8dba9187323b28ae95c
TheFinalTzar <44308833+TheFinalTzar@users.noreply.github.com>
2018-10-20 15:41:19 -0500

    Reclaimed lost territory from the gremlins

--- app/src/main/java/mealmarch/com/mealmarchtest/MyActivity.java
+++ app/src/main/java/mealmarch/com/mealmarchtest/MyActivity.java
@@ -1,34 +1,35 @@
 package mealmarch.com.mealmarchtest;
 
-        import android.support.annotation.NonNull;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-        import com.google.android.gms.tasks.OnCompleteListener;
-        import com.google.android.gms.tasks.OnFailureListener;
-        import com.google.android.gms.tasks.OnSuccessListener;
-        import com.google.android.gms.tasks.Task;
-        import com.google.firebase.firestore.DocumentReference;
-        import com.google.firebase.firestore.FirebaseFirestore;
-        import com.google.firebase.firestore.QueryDocumentSnapshot;
-        import com.google.firebase.firestore.QuerySnapshot;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-        import java.io.BufferedReader;
-        import java.io.InputStreamReader;
-        import java.net.URL;
-        import java.util.HashMap;
-        import java.util.Map;
+import org.json.JSONObject;
 
-        import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MyActivity extends AppCompatActivity {
 
-    private static String APIKey = "AIzaSyDsc00rP6Gf0HLpoVmz_onQMynA_Z5ErmA";
-    private static final String TAG = "MainActivity";
+    private static String APIKey = "AIzaSyDqDkgbIeELCnh6ek8QCm-ROu_rd1SkydU";
     Button searchBtn;
+    TextView result;
+    private String searchResult;
+    private String[][] places;
 
 
 
@@ -38,7 +39,7 @@ public class MyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-
+        places = new String[20][10];
         searchBtn  = (Button)findViewById(R.id.searchFoodBtn);
         searchBtn.setOnClickListener(new View.OnClickListener() {
             //creates button calling functionality
@@ -47,6 +48,7 @@ public class MyActivity extends AppCompatActivity {
                 searchFood(v);
             }
         });
+        result = (TextView)(findViewById(R.id.pageText));
 
 
     }
@@ -57,24 +59,48 @@ public class MyActivity extends AppCompatActivity {
 
         String location, radius;
         //temporary dummy data
-        radius = "2000";
-        location = "41.981950, -91.663183";
-
+        radius = "5000";
+        location = "41.981950,-91.663183";
+        result.setText("Hello");
         try{
-            String searchString = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?location="+location+"&radius="+radius+"&type=restaurant&key="+APIKey;
-            URL url = new URL(searchString);
-            HttpsURLConnection connection = (HttpsURLConnection)(url.openConnection());
-            //int responseCode = connection.getResponseCode();
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuffer responseBuffer = new StringBuffer();
-            while ((line = in.readLine()) != null){
-                responseBuffer.append(line);
-            }
-            response = responseBuffer.toString();
+
+
+
+            String searchString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+location+"&radius="+radius+"&type=restaurant&key="+APIKey;
+//            URL url = new URL(searchString);
+//            RequestQueue requestQueue = Volley.newRequestQueue(this);
+//            StringRequest stringRequest = new StringRequest(Request.Method.GET, searchString,
+//                    new Response.Listener<String>() {
+//                        @Override
+//                        public void onResponse(String response) {
+//                            result.setText("Hello there");
+//                        }
+//                    }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    result.setText("Bad bad bad");
+//                }
+//            });
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, searchString, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    searchResult = response.toString();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    result.setText("Bad bad bad");
+                }
+            });
+
+            requestQueue.add(jsonObjectRequest);
+            result.setText(searchResult);
+
+
         }
         catch(Exception e){
-            //some exception handling
+            result.setText(e.toString());
         }
     }
 
