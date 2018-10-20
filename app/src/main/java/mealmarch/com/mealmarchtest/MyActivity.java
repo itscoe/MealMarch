12cdb59de5350f5d347151c6e16414f7580b6c82
TheFinalTzar <44308833+TheFinalTzar@users.noreply.github.com>
2018-10-20 01:09:10 -0500

    Added url creation to be queried

--- app/src/main/java/mealmarch/com/mealmarchtest/MyActivity.java
+++ app/src/main/java/mealmarch/com/mealmarchtest/MyActivity.java
@@ -1,15 +1,25 @@
 package mealmarch.com.mealmarchtest;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import java.net.URL;
 
 public class MyActivity extends AppCompatActivity {
 
     private static String APIKey = "AIzaSyDsc00rP6Gf0HLpoVmz_onQMynA_Z5ErmA";
+    private static String searchString = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?location=41.981950, -91.663183&radius=2000&type=restaurant&key="+APIKey;
 
+    private String result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        try{
+            URL url = new URL(searchString);
+        }
+        catch(Exception e){
+
+        }
     }
 }
