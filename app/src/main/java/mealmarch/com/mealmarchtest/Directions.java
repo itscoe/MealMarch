52977615ecdfc9e1680f43c253bb07f6b2ae6eef
itscoe <itslagle@coe.edu>
2018-10-21 00:41:32 -0500

    Easy Mode passes to results

--- app/src/main/java/mealmarch/com/mealmarchtest/Directions.java
+++ app/src/main/java/mealmarch/com/mealmarchtest/Directions.java
@@ -1,13 +1,18 @@
 package mealmarch.com.mealmarchtest;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
 
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.GeoPosition;
@@ -24,6 +29,8 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.google.common.reflect.Reflection.initialize;
 
@@ -37,6 +44,10 @@ public class Directions extends AppCompatActivity {
     boolean hasRoute = false;
     MapFragment mapFragment;
     Map map;
+    MapRoute mapRoute;
+    int time = 0;
+    double initialDistance;
+
 
     /**
      * permissions request code
@@ -104,7 +115,7 @@ public class Directions extends AppCompatActivity {
             // If the route was calculated successfully
             if (error == RouteManager.Error.NONE) {
                 // Render the route on the map
-                MapRoute mapRoute = new MapRoute(routeResult.get(0).getRoute());
+                mapRoute = new MapRoute(routeResult.get(0).getRoute());
                 map.addMapObject(mapRoute);
             } else {
                 System.out.println("Could not find route");
@@ -116,6 +127,7 @@ public class Directions extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions);
+        checkPermissions();
         String location = getIntent().getStringExtra("loc");
         System.out.println("Location: " + location);
         String[] coordinates = location.split(",");
@@ -150,6 +162,35 @@ public class Directions extends AppCompatActivity {
                 }
             }
         });
+        Button Btn = (Button)findViewById(R.id.button);
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnClick();
+            }
+        });
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // task to run goes here
+                time++;
+            }
+        };
+        Timer timer = new Timer();
+        long delay = 0;
+        long intevalPeriod = 1000;
+        // schedules the task to be run in an interval
+        timer.scheduleAtFixedRate(task, delay,
+                intevalPeriod);
+    }
+
+    private void btnClick() {
+        Intent i = new Intent(this, Results.class);
+        i.putExtra("distance", initialDistance);
+        i.putExtra("time", time);
+        String name = getIntent().getStringExtra("name");
+        i.putExtra("name", name);
+        startActivity(i);
     }
 
     private PositioningManager.OnPositionChangedListener positionListener = new
@@ -164,7 +205,9 @@ public class Directions extends AppCompatActivity {
                         routePlan.addWaypoint(restaurant);
                         rm.calculateRoute(routePlan, new RouteListener());
                         hasRoute = true;
+                        initialDistance = geoPosition.getCoordinate().distanceTo(restaurant);
                     }
+                    map.addMapObject(mapRoute);
                 }
 
                 @Override
