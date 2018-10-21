705ec9bebdaa515b8c082fa9981992a83aba1662
ebford <38540745+ebford@users.noreply.github.com>
2018-10-21 01:32:39 -0500

    updating text box for feedback

--- app/src/main/java/mealmarch/com/mealmarchtest/Feedback.java
+++ app/src/main/java/mealmarch/com/mealmarchtest/Feedback.java
@@ -2,12 +2,30 @@ package mealmarch.com.mealmarchtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class Feedback extends AppCompatActivity {
+
+    RatingBar ratingBar;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        ratingBar = findViewById(R.id.imageBar);
+        tv = findViewById(R.id.Rating);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Log.d("uuu",ratingBar.getRating()+"");
+                tv.setText(String.valueOf(ratingBar.getRating()));
+            }
+        });
+
     }
 }
